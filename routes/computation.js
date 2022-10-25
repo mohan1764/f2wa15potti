f58ce1d76212e91e7857d4224d1cd6835d2e52da
mohan1764 <s554815@nwmissouri.edu>
2022-10-24 20:06:36 -0500

    favourite musedum

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,9 @@
-var express = require('express');\
+var express = require('express');
 var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     fake_url = "https://fake.com/path" + req.url
-    const url = new URL(fake_url)\
+    const url = new URL(fake_url)
     const search_params = url.searchParams
     if (req.method === 'GET') {
         value = search_params.get("x")
